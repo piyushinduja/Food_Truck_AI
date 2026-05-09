71732f64c7e2df61409e9cbfc56547bc5c54d774
piyushinduja <piyushinduja47@gmail.com>
2026-05-09 15:15:15 -0600

    Fixed streamlit app column issue

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,7 +27,7 @@ st.set_page_config(
     page_title="El Camino",
     page_icon="🛣️",
     layout="wide",
-    initial_sidebar_state="collapsed",
+    initial_sidebar_state="expanded",
 )
 
 ensure_app_ready()
